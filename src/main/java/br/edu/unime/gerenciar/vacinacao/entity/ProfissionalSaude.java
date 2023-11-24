501c81957efe5ed8ff4f389e20a00be97fce1b8f
mariojbe <mariojbe@gmail.com>
2023-11-24 15:29:55 -0300

    Update ProfissionalSaude.java

--- src/main/java/br/edu/unime/gerenciar/vacinacao/entity/ProfissionalSaude.java
+++ src/main/java/br/edu/unime/gerenciar/vacinacao/entity/ProfissionalSaude.java
@@ -21,6 +21,7 @@ public class ProfissionalSaude {
     private String id;
 
     @NotBlank
+    @NotNull
     @Size(min = 3, max = 100, message = "o nome deve ter entre 3 e 100 digitos!")
     private String nome;
 
